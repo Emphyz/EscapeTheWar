e181c79306c5195c44c4a74bff83d555eff24222
Emphys <viviteemo@gmail.com>
2022-03-06 13:37:32 +0100

    new useless comment in bob

--- src/bob_le_chef_dorchestre.py
+++ src/bob_le_chef_dorchestre.py
@@ -1,5 +1,8 @@
 from ursina import *
 from scene import *
 
-def bob(nom_instance, new_entities:Dict[Entity]):
+
+def bob(nom_instance, new_entities: Dict[Entity]):
     nom_instance.set_entities(new_entities)
+
+# vhdtuf
